3190562a0fc1315f6dbdd9392a0d7bc17c1a91b4
Aroush-log <malikaroush2011@gmail.com>
2026-07-09 23:42:29 +0400

    Added first action breakdown feature

---
+++ script.js
@@ -0,0 +1,8 @@
+function breakDown() {
+  let problem = document.getElementById("problem").value;
+
+  document.getElementById("result").innerHTML =
+  "1. Write down the biggest task first.<br>" +
+  "2. Break it into smaller steps.<br>" +
+  "3. Pick one small action to start with.";
+}
